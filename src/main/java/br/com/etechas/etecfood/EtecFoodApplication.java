ecacb29d2ef1a48a3beac3558596be01250f68e6
Eliane <eliane.rosa@etec.sp.gov.br>
2025-09-09 09:21:02 -0300

    Projeto etec-food: commit inicial - criação do projeto 2AI

---
+++ src/main/java/br/com/etechas/etecfood/EtecFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.etechas.etecfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtecFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtecFoodApplication.class, args);
+	}
+
+}
