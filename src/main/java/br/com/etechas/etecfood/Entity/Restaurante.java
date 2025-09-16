80ed6447a862d8ff3d2c135632d3e322c19b1f6c
iMatechh <matheus_rochaalves@hotmail.com>
2025-09-16 08:13:00 -0300

    TipoCozinhaEnum e Restaurante JAVA pt.1

---
+++ src/main/java/br/com/etechas/etecfood/Entity/Restaurante.java
@@ -0,0 +1,19 @@
+package br.com.etechas.etecfood.Entity;
+
+public class Restaurante {
+    private Long id;
+    private String cnpj;
+    private String nome;
+    private String descricao;
+    private String cep;
+    private String endereco;
+    private Double taxaDeEntrega;
+    private Integer tempoDeEntregaMinimo;
+    private Integer tempoDeEntregaMaximo;
+    private boolean aprovado;
+
+    private TipoCozinhaEnum tipoDeCozinha;
+    private Usuario usuario;
+
+
+}
---
+++ src/main/java/br/com/etechas/etecfood/enums/TipoCozinhaEnum.java
@@ -0,0 +1,14 @@
+package br.com.etechas.etecfood.enums;
+
+public enum TipoCozinhaEnum {
+    CHINESA,
+    JAPONESA,
+    MEXICANA,
+    MINEIRA,
+    BAIANA,
+    LANCHES,
+    HAMBURGER,
+    ARABE,
+    ITALIANA,
+    VARIADA;
+}
