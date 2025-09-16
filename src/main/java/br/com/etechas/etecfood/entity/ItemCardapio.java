89248329cbae2213365108bdd3bc6ff15b3d84ce
etechas <gchaves961@gmail.com>
2025-09-16 08:50:33 -0300

    Criacao da classe ItemCardapio.java

---
+++ src/main/java/br/com/etechas/etecfood/entity/ItemCardapio.java
@@ -0,0 +1,18 @@
+package br.com.etechas.etecfood.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@Entity
+
+@Table(name = "TBL_ITEM_CARDAPIO")
+public class ItemCardapio {
+    @Id
+    @Column(name = "ID_ITEM_CARDAPIO")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
