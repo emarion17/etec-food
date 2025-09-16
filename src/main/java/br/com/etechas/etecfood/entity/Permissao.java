9ed2929adc517f0ef4a7a34d7f9e36e3b9b33114
hEiToR232 <heitor.sousa5@etec.sp.gov.br>
2025-09-16 08:00:34 -0300

    mapeamento do banco

--- src/main/java/br/com/etechas/etecfood/entity/Permissao.java
+++ src/main/java/br/com/etechas/etecfood/entity/Permissao.java
@@ -1,5 +1,15 @@
 package br.com.etechas.etecfood.entity;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Permissao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+
     private String authority;
 }
