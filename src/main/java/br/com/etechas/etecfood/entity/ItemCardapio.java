39d91f465677c91de83459f2cec9b16f593a170e
migueld072 <migueldspoliveira@gmail.com>
2025-11-15 19:15:48 -0300

    itemcardapio do java

--- src/main/java/br/com/etechas/etecfood/entity/ItemCardapio.java
+++ src/main/java/br/com/etechas/etecfood/entity/ItemCardapio.java
@@ -6,13 +6,32 @@ import lombok.Setter;
 
 @Getter
 @Setter
-
 @Entity
-
 @Table(name = "TBL_ITEM_CARDAPIO")
 public class ItemCardapio {
     @Id
     @Column(name = "ID_ITEM_CARDAPIO")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo")
+    private TipoItem tipo;
+
+    @Column(name = "preco")
+    private Double preco;
+
+    @Column(name = "preco_promocional")
+    private Double precoPromocional;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_cardapio")
+    private Cardapio cardapio;
+
 }
