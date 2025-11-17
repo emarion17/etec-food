c678348896b053adffc73359f90f818ff704ab11
Giulia <giuliasouzaresende0809@gmail.com>
2025-11-16 22:53:56 -0300

    Buscar em cardapio por categoria

--- src/main/java/br/com/etechas/etecfood/repository/CardapioRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/CardapioRepository.java
@@ -4,6 +4,9 @@ import br.com.etechas.etecfood.entity.Cardapio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardapioRepository extends JpaRepository<Cardapio, Long> {
+    List<Cardapio> findByCategoria(String categoria);
 }
