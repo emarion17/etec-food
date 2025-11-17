2a10d3ae4f29bc544406f429cdd3fc38e0b170b2
Bibia-07 <bibica.pc2020@gmail.com>
2025-11-17 10:58:51 -0300

    Implement ItemCardapioController with CRUD operations

--- src/main/java/br/com/etechas/etecfood/controller/ItemCardapioController.java
+++ src/main/java/br/com/etechas/etecfood/controller/ItemCardapioController.java
@@ -1,1 +1,39 @@
+package br.com.etechas.etecfood.controller
 
+import br.com.etechas.ingresso.entity.Filme;
+import br.com.etechas.ingresso.repository.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/itemCardapios")
+
+    public class ItemCardapioController {
+
+        @Autowired
+        private ItemCardapioRepository itemCardapioRepository;
+
+        @GetMapping
+        public List <ItemCardapio> listar() {return itemCardapioRepository.findAll();}
+
+         @GetMapping("/{Id}")
+        public ItemCardapio BuscarPorId(@PathVariable Long Id){
+        var itemCardapio = itemCardapioRepository.findById(Id);
+        if(itemCardapio.isPresent()){
+            return itemCardapio.get();
+        }
+        return null;
+    }
+        @PostMapping 
+        public void cadastrar(@RequestBody ItemCardapio itemCardapio) {itemCardapioRepository.save(itemCardapio); }
+
+        @DeleteMapping
+        public void excluir (@PathVariable Long id){
+            var existe = itemCardapioRepository.findById(id);
+            if(existe.isPresent()){
+                itemCardapioRepository.delete(existe.get());
+            }
+        }
+    }
