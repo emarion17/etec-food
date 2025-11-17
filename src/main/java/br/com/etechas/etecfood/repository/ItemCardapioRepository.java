8b21a8500f3e8258d62ef3803f52a6506ec00fc9
Bibia-07 <bibica.pc2020@gmail.com>
2025-11-17 10:47:48 -0300

    Remove unused method from ItemCardapioRepository
    Removed unused method declaration for findByNome.

--- src/main/java/br/com/etechas/etecfood/repository/ItemCardapioRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/ItemCardapioRepository.java
@@ -2,12 +2,10 @@ package br.com.etechas.etecfood.repository;
 
 import br.com.etechas.etecfood.entity.ItemCardapio;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
 @Repository
 public interface ItemCardapioRepository extends JpaRepository<ItemCardapio, Long> {
-    List<ItemCardapio> findByNome(String nome);
-
 }
