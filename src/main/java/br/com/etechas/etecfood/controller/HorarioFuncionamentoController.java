1ac33409bd2966efa8ad4b7c3418da895f6d1254
sarahvf1406 <sarahferreira140616@gmail.com>
2025-11-18 20:01:40 -0300

    adição do autowired e getmapping listar no HorarioFuncionamentoController

--- src/main/java/br/com/etechas/etecfood/controller/HorarioFuncionamentoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/HorarioFuncionamentoController.java
@@ -1,4 +1,24 @@
 package br.com.etechas.etecfood.controller;
 
+import br.com.etechas.etecfood.entity.HorarioFuncionamento;
+import br.com.etechas.etecfood.repository.HorarioFuncionamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/horarios")
 public class HorarioFuncionamentoController {
+
+
+    @Autowired
+    private HorarioFuncionamentoRepository horarioFuncionamentoRepository;
+
+    @GetMapping
+    public List<HorarioFuncionamento> listar() {
+        return horarioFuncionamentoRepository.findAll();
+    }
 }
