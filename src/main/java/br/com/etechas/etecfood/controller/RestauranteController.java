57818f1e9537dd6d671d1455d2de3e44c97d7b2d
darihk <brunoabreu55@gmail.com>
2025-11-18 19:28:12 -0300

    Adição em RestauranteController: GetMapping buscarRestauranteId e PostMapping cadastrarRestaurante

--- src/main/java/br/com/etechas/etecfood/controller/RestauranteController.java
+++ src/main/java/br/com/etechas/etecfood/controller/RestauranteController.java
@@ -3,9 +3,8 @@ package br.com.etechas.etecfood.controller;
 import br.com.etechas.etecfood.entity.Restaurante;
 import br.com.etechas.etecfood.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +19,16 @@ public class RestauranteController {
     public List<Restaurante> listar(){
         return restauranteRepository.findAll();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Restaurante> buscarRestauranteId(@PathVariable Long id){
+        var restaurante = restauranteRepository.findById(id);
+        if(restaurante.isPresent()){
+            return ResponseEntity.ok(restaurante.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public void cadastrarRestaurante(@RequestBody Restaurante restaurante){restauranteRepository.save(restaurante);}
 }
