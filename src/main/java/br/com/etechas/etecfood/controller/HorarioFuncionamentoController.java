40bc3d42d9a33f2d3c3778bbbf85c32c3cf14dc5
sarahvf1406 <sarahferreira140616@gmail.com>
2025-11-18 20:18:06 -0300

    adição do postmapping cadastrar no HorarioFuncionamentoController

--- src/main/java/br/com/etechas/etecfood/controller/HorarioFuncionamentoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/HorarioFuncionamentoController.java
@@ -4,10 +4,7 @@ import br.com.etechas.etecfood.entity.HorarioFuncionamento;
 import br.com.etechas.etecfood.repository.HorarioFuncionamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,5 +30,12 @@ public class HorarioFuncionamentoController {
         return ResponseEntity.notFound().build();
 
     }
+
+
+    @PostMapping
+    public void cadastrar(@RequestBody HorarioFuncionamento horario) {
+        horarioFuncionamentoRepository.save(horario);
+    }
+
 }
 
