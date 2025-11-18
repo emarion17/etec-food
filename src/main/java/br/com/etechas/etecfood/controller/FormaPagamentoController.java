6d581bcaa2cbff6e28a1b88bbf15f6a509c84edd
KamilaMorais <kamilamorais.ctt@gmail.com>
2025-11-18 20:05:11 -0300

    commit1

---
+++ src/main/java/br/com/etechas/etecfood/controller/FormaPagamentoController.java
@@ -0,0 +1,15 @@
+package br.com.etechas.etecfood.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RequestController
+@RequestMapping({"/formaPagamento"})
+
+public class FormaPagamentoController {
+    @Autowired
+    private FormaPagamentoRepository formaPagamentoRepository;
+}
