f44af528f8e2ca1f55d5afaf865dc272cb7b7035
fefocardozo <cardozofeeh808@gmail.com>
2025-11-19 07:46:37 -0300

    extender

--- src/main/java/br/com/etechas/etecfood/repository/FormaPagamentoRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/FormaPagamentoRepository.java
@@ -3,5 +3,5 @@ package br.com.etechas.etecfood.repository;
 import br.com.etechas.etecfood.entity.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FormaPagamentoRepository{
+public interface FormaPagamentoRepository extends JpaRepository{
 }
