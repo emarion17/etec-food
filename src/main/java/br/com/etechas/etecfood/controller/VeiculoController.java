6719d6dd2736081a8d06480f93264337521cdbb0
Duncan_TCK <nickolasericardogit@gmail.com>
2025-11-19 07:58:28 -0300

    VeiculoController incompleto adicionado (id) (Feito por: Ricardo)

--- src/main/java/br/com/etechas/etecfood/controller/VeiculoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/VeiculoController.java
@@ -1,9 +1,15 @@
 package br.com.etechas.etecfood.controller;
 
+import br.com.etechas.etecfood.entity.Veiculo;
 import br.com.etechas.etecfood.repository.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Veiculos")
@@ -11,5 +17,19 @@ public class VeiculoController {
 
     @Autowired
     private VeiculoRepository veiculoRepository;
+    @GetMapping
+    public List<Veiculo> listar(){
+        return veiculoRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Veiculo> buscarPorId(@PathVariable Long id){
+        var veiculo = veiculoRepository.findById(id);
+        if(veiculo.isPresent()){
+            return ResponseEntity.ok(veiculo.get());
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
