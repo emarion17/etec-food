e87631863befd3817ca673ac201b3917429325e1
Duncan_TCK <nickolasericardogit@gmail.com>
2025-11-19 08:12:57 -0300

    Aplicação funcionando (Feito por: Matheus)

--- src/main/java/br/com/etechas/etecfood/controller/VeiculoController.java
+++ src/main/java/br/com/etechas/etecfood/controller/VeiculoController.java
@@ -7,9 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/Veiculos")
+@RequestMapping("/veiculos")
 public class VeiculoController {
 
     @Autowired
