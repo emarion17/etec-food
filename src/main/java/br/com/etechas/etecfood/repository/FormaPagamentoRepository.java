9ac2c5ce9cf8a8760e6317f9016117bf596205c0
fefocardozo <cardozofeeh808@gmail.com>
2025-11-19 07:45:55 -0300

    criação do FormaPagamentoRepository

---
+++ src/main/java/br/com/etechas/etecfood/repository/FormaPagamentoRepository.java
@@ -0,0 +1,7 @@
+package br.com.etechas.etecfood.repository;
+
+import br.com.etechas.etecfood.entity.FormaPagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FormaPagamentoRepository{
+}
