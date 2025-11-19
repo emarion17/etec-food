f1e0671e4e8bba49c4bbd50a58eeb1dfa64bef0d
Duncan_TCK <nickolasericardogit@gmail.com>
2025-11-19 07:45:08 -0300

    EntregadorRepository adicionado (Feito por: Ricardo)

---
+++ src/main/java/br/com/etechas/etecfood/repository/EntregadorRepository.java
@@ -0,0 +1,7 @@
+package br.com.etechas.etecfood.repository;
+
+import br.com.etechas.etecfood.entity.Entregador;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntregadorRepository extends JpaRepository<Entregador, Long> {
+}
