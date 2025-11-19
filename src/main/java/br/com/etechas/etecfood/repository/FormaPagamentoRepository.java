5892f7c661b053070b9066ac2da425cb8c297dd9
fefocardozo <cardozofeeh808@gmail.com>
2025-11-19 07:53:44 -0300

    tirei o espaço

--- src/main/java/br/com/etechas/etecfood/repository/FormaPagamentoRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/FormaPagamentoRepository.java
@@ -3,5 +3,5 @@ package br.com.etechas.etecfood.repository;
 import br.com.etechas.etecfood.entity.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FormaPagamentoRepository extends JpaRepository <FormaPagamento, Long> {
+public interface FormaPagamentoRepository extends JpaRepository <FormaPagamento, Long>{
 }
