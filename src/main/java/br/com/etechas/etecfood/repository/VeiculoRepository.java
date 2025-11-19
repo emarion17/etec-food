c2450508461c9ee1058b6b00905d9f403e61722b
Duncan_TCK <nickolasericardogit@gmail.com>
2025-11-19 07:35:43 -0300

    VeiculoRepository adicionado (Feito por: Nickolas)

--- src/main/java/br/com/etechas/etecfood/repository/VeiculoRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/VeiculoRepository.java
@@ -3,5 +3,6 @@ package br.com.etechas.etecfood.repository;
 import br.com.etechas.etecfood.entity.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+public interface VeiculoRepository extends JpaRepository <Veiculo, Long> {
+
 }
