e41ed33afc6f30c02282a86237b880951c28e1ea
Duncan_TCK <nickolasericardogit@gmail.com>
2025-11-19 07:39:38 -0300

    VeiculoRepository adicionado (Feito por: Nickolas)

---
+++ src/main/java/br/com/etechas/etecfood/entity/Entregador.java
@@ -0,0 +1,26 @@
+package br.com.etechas.etecfood.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+    @Getter
+    @Setter
+    @Entity
+    @Table(name = "TBL_ENTREGADOR")
+    public class Entregador {
+        @Id
+        @Column(name ="ID_ENTREGADOR")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(name ="NOME")
+        private String nome;
+
+        @Column(name ="TELEFONE")
+        private String telefone;
+
+        @Column(name ="ATIVO")
+        private Boolean ativo;
+}
