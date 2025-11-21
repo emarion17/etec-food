ef0534fb1a245028c163d622458ae863a44a4cf0
ThiagoCL16 <thiagocavalcantelopes16@gmail.com>
2025-11-21 11:28:10 -0300

    Commits do Felipe Santos (desconsiderar na contagem dos meus commits)

---
+++ src/main/java/br/com/etechas/etecfood/repository/ItemPedidoRepository.java
@@ -0,0 +1,7 @@
+package br.com.etechas.etecfood.repository;
+
+import br.com.etechas.etecfood.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+}
